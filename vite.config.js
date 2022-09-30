4c7eee8c818e0f1709664c241417f8042ad36fdc
JoJosSidekick <114755556+JoJoSidekick@users.noreply.github.com>
2022-09-30 12:16:02 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/site/',  //this is base url of your website
+  base:'/sk/',  //this is base url of your website
   plugins: [react()]
 })
